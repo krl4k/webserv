eddc579ecee1653ccb69b0d9e78e889538c88edc
mwinter <atrumksar@gmail.com>
2021-04-10 20:50:42 +0300

    "get IPadress IPv4 or IPv6"

--- test.cpp
+++ test.cpp
@@ -3,35 +3,54 @@
 //
 
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <netdb.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-void ft_strcpy(char *dest, const char *src)
-{
-//	printf("dest  = %p\n", &dest);
-	std::cout << "dest = " << &dest << std::endl;
+int main(int argc, char *argv[]) {
 
-	size_t i;
+	struct addrinfo hints, *res, *p;
+	int status;
+	char ipstr[INET6_ADDRSTRLEN];
 
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
+	if (argc != 2) {
+		std::cerr << "Usage: showip hostnamen" << std::endl;
+		return 1;
 	}
-	dest[i] = '\0';
-//	return (dest);
-}
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	if ((status = getaddrinfo(argv[1], NULL, &hints, &res))) {
+		std::cerr << "getaddrinfo: " + std::string(gai_strerror(status)) << std::endl;
+		return 2;
+	}
+	std::cout << "IP address for: " + std::string(argv[1]) << std::endl;
+	for (p = res; p != nullptr; p = p->ai_next) {
+		void *addr;
+		std::string ipver;
 
-int main()
-{
-	char *dest = (char *)malloc(10);
-//	printf("dest  = %p\n", &dest);
-	std::cout << "dest = " << &dest << std::endl;
-
-	ft_strcpy(dest, "qwertyuio\0");
-
-
-//	char srcs[10];
-//	ft_strcpy(srcs, dest);
-	std::cout << "copy = " << dest << std::endl;
-	return (0);
+		if (p->ai_family == AF_INET) {
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+		} else {
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "IPv6";
+		}
+		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+		std::cout << ipver << ": " << ipstr << std::endl;
+	}
+	freeaddrinfo(res);
+	return 0;
 }
