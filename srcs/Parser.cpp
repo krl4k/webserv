fcb2235a75d1e3ab62c2589ef1b5263b7ba49a90
itollett <itollett@student.21-school.ru>
2021-04-18 21:02:39 +0300

    hello dummies1

--- srcs/Parser.cpp
+++ srcs/Parser.cpp
@@ -24,7 +24,7 @@ Parser::Parser(const std::string &fileName) {
 	if (!file.is_open()){
 		throw std::runtime_error("file corrupted");
 	}
-	bigLine = this->readFile(file);
+		bigLine = this->readFile(file);
 	file.close();
 
 }
@@ -50,6 +50,10 @@ std::string Parser::readFile(std::ifstream &fd) {
 				o.clear();
 			}
 		}
+		else if ((buf.find("server:") != std::string::npos && (buf.compare(0, 7, "server:")) && buf.size() != 7)) {
+			throw std::runtime_error("config: server line error");
+			//std::cout << "hello mfc " << std::endl;
+		}
 		o << buf << std::endl;
 		temp = o.str();
 	}
@@ -59,19 +63,24 @@ std::string Parser::readFile(std::ifstream &fd) {
 	return (o.str());
 }
 
-std::string getmyline(std::string& str, std::string com){
+std::string getmyline(std::string& str, std::string com, int n){
 	std::string temp;
 	int i = 0;
-	while (str[i] == ' ' || str[i] == '\t'){
+	while (str[i] == '\t'){
 		i++;
+	}
+	if (i != n){
+		throw std::runtime_error("Tabulation error");
+	}
+	if (str[i + 1] && str.compare(i, com.size(), com)){
+		throw std::runtime_error("Config file error");
 	}
 	temp = str.substr(i + com.size() + 1, str.size() - i);
 	if (temp.find(com.c_str(), 0, com.size() - 1)){
 		return (temp);
 	}
 	else{
-		temp = "Error";
-		return (temp);
+		throw std::runtime_error("Config file error");
 	}
 }
 
@@ -81,23 +90,26 @@ int Parser::initLocations(std::vector<std::string> &strings, int count, int i){
 	std::map<std::string, Location> locMap;
 	while (count < strings.size()){
 		if (strings[count].find("location:") != std::string::npos){
-			temp = getmyline(strings[count],  "location:");
+			temp = getmyline(strings[count],  "location:", 2);
 			count++;
 			Location *newLoc = new Location();
 			while(strings[count].find("location:") == std::string::npos && count < strings.size()){
-				if (strings[count] == "\n")
+				if (strings[count] == "\n" || strings[count] == "")
 					;
 				else if (strings[count].find("root:") != std::string::npos){
-					newLoc->setRoot(getmyline(strings[count], "root:"));
+					newLoc->setRoot(getmyline(strings[count], "root:", 3));
 				}
 				else if (strings[count].find("access-control-allow-methods:") != std::string::npos){
-					newLoc->setAllowMethods(getmyline(strings[count], "access-control-allow-methods:"));
+					newLoc->setAllowMethods(getmyline(strings[count], "access-control-allow-methods:", 3));
 				}
 				else if (strings[count].find("index:") != std::string::npos){
-					newLoc->setRoot(getmyline(strings[count], "index:"));
+					newLoc->setRoot(getmyline(strings[count], "index:", 3));
 				}
 				else if (strings[count].find("cgi_path:") != std::string::npos){
-					newLoc->setCgiPath(getmyline(strings[count], "cgi_path:"));
+					newLoc->setCgiPath(getmyline(strings[count], "cgi_path:",3));
+				}
+				else{
+					throw std::runtime_error("Config parser error");
 				}
 				count++;
 			}
@@ -124,19 +136,19 @@ Server * Parser::separateServers(std::string &line, size_t i) {
 	}
 	int count = 1;
 	while (strings[count].find("location:") == std::string::npos && count != strings.size()){
-		if (strings[count] == "\n")
+		if (strings[count] == "\n" || strings[count] == "")
 			;
 		else if (strings[count].find("host:") != std::string::npos){
-			newServ->setHost(getmyline(strings[count], "host:"));
+			newServ->setHost(getmyline(strings[count], "host:", 1));
 		}
 		else if (strings[count].find("port:") != std::string::npos){
-			newServ->setPort(getmyline(strings[count], "port:"));
+			newServ->setPort(getmyline(strings[count], "port:",1));
 		}
 		else if (strings[count].find("server_name:") != std::string::npos){
-			newServ->setServerName(getmyline(strings[count], "server_name:"));
+			newServ->setServerName(getmyline(strings[count], "server_name:", 1));
 		}
 		else if (strings[count].find("error_page:") != std::string::npos){
-			newServ->setErrorPage(getmyline(strings[count], "error_page:"));
+			newServ->setErrorPage(getmyline(strings[count], "error_page:", 1));
 		}
 		else{
 			std::cout << "Error in the parser" << std::endl;
@@ -149,5 +161,7 @@ Server * Parser::separateServers(std::string &line, size_t i) {
 	} else{
 		std::cout << "there is not locations" << std::endl;
 	}
+	if (count != strings.size())
+		throw std::runtime_error("Config file error");
 	return (_servers[i]);
 }
