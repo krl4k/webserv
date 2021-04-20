effd766d4dbfcda422e9b1d015199aab9c136372
itollett <itollett@student.21-school.ru>
2021-04-20 13:15:58 +0300

    parser validator

--- includes/HttpResponse.hpp
+++ includes/HttpResponse.hpp
@@ -14,6 +14,7 @@
 #include <sstream>
 #include <unistd.h>
 #include <fcntl.h>
+#include <ctime>
 
 class HttpResponse {
 public:
--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 
-#include "includes/WebServer.hpp"
+//#include "includes/WebServer.hpp"
+#include <ctime>
 
 int main(int argc, char **argv) {
-	if (argc <= 2){
-		try {
-			WebServer webServer(argv[1]);
-			webServer.start();
-		} catch (std::exception &exception) {
-			std::cerr << "Server dont started!" << std::endl;
-		}
-	} else {
-		std::cerr << "Wrong arguments!" << std::endl;
-		return (-1);
-	}
+
 	return (0);
 }
 
