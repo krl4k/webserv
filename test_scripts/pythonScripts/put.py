f0e733d03e7d7daa43a267f63ab64fb664c15843
krl4k <fhds.hdd@mail.ru>
2021-04-23 10:52:06 +0300

    new python scripts

---
+++ test_scripts/pythonScripts/put.py
@@ -0,0 +1,17 @@
+import requests as req
+
+contLen = 123445
+def generator():
+    v1 = ("ab" * contLen)
+    yield v1.encode('utf8')
+
+url = 'http://localhost:8000/'
+
+# count user
+counUser = 200
+for i in range(0, counUser):
+    print("==============================================================")
+    r = req.put(url)
+    print("Count    = " + str(i))
+    print("Content  = " + r.text)
+    print("Response = " + str(r.status_code))
